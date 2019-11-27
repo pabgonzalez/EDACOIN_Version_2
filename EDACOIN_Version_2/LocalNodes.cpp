5db3c764d1e3e827085569d8ebbb6fc6fe1d3618
Nico Pelayo <nifernandez@itba.edu.ar>
2019-11-27 15:43:27 -0300

    Primer version de grafico de grafos

--- EDACOIN_Version_2/LocalNodes.cpp
+++ EDACOIN_Version_2/LocalNodes.cpp
@@ -1,4 +1,16 @@
 #include "LocalNodes.h"
+#include <math.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
+
+#define RAD 120
+#define THICKNESS 10
+#define FONT_SIZE 20
+#define SIZEOFGRAPH 1000
+#define PX_PER_UNIT (SIZEOFGRAPH/5)
+
 
 LocalNodes::LocalNodes() {
 	selectedNodeIndex = -1;
@@ -51,4 +63,26 @@ void LocalNodes::appendNode(FullNode* node) {
 void LocalNodes::appendNode(SPVNode* node) {
 	spvNodes.push_back(SPVNodeInfo(node));
 	notifyAllObservers();
+}
+
+ALLEGRO_BITMAP* FullNodeInfo::graphNeighbours(void) {
+	ALLEGRO_BITMAP* backup = al_get_target_bitmap();
+	ALLEGRO_FONT* font = NULL;
+	ALLEGRO_BITMAP* graph = NULL;
+	char fontLoc[] = "arial.ttf";
+	graph = al_create_bitmap(PX_PER_UNIT * 5, PX_PER_UNIT * 5);
+	al_set_target_bitmap(graph);
+	al_clear_to_color(al_map_rgb(255, 255, 255));
+	font = al_load_ttf_font(fontLoc, FONT_SIZE, 0);
+	map<string, SocketType>::iterator it = fullNode->getNeighbours().begin();
+	al_draw_filled_circle(PX_PER_UNIT * 5 / 2, PX_PER_UNIT * 5 / 2, 60, al_map_rgb(100, 255, 100));
+	al_draw_text(font, al_map_rgb(0, 0, 0), (PX_PER_UNIT * 5 / 2) , (PX_PER_UNIT * 5 / 2),NULL,fullNode->getNodeID().c_str());
+	for (int i = 0; i <= (fullNode->getNeighbours().size()); i++, it++) {
+		double angle = i * 2 * 3.14 / fullNode->getNeighbours().size();
+		al_draw_filled_circle((PX_PER_UNIT * 5 / 2) + RAD * cos(angle), (PX_PER_UNIT * 5 / 2) - RAD * sin(angle), 60, al_map_rgb(100, 255, 100));
+		al_draw_line((PX_PER_UNIT * 5 / 2), (PX_PER_UNIT * 5 / 2),(PX_PER_UNIT * 5 / 2) + RAD * cos(angle), (PX_PER_UNIT * 5 / 2) - RAD * sin(angle), al_map_rgb(255, 0, 0), THICKNESS);
+		al_draw_text(font,al_map_rgb(0,0,0),(PX_PER_UNIT * 5 / 2) + RAD * cos(angle), (PX_PER_UNIT * 5 / 2) - RAD * sin(angle),NULL, it->first.c_str());
+	}
+	al_set_target_bitmap(backup);
+	return graph;
 }
